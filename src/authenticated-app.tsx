c3597714688289ab32cebf39558b5fa4327a470d
yojigoji <gojie414@gmail.com>
2022-05-30 09:12:59 +0800

    style: 用styled写样式

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,63 +1,40 @@
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <PageHeader>
-        <HeaderLeft>123</HeaderLeft>
+      <PageHeader between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </PageHeader>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
 
-const PageHeader = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+const PageHeader = styled(Row)`
   justify-content: space-between;
 `;
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
 
-const Main = styled.main`
-  grid-area: main;
-`;
-
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
 
 const Container = styled.div`
   display: grid;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-rows: 6rem 1fr 6rem;
-  height: 100ch;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
 `;
